a9be9afa862720359a2244514438a05e4261c7e3
robin.hermansson <robin.hermansson@hotmail.se>
2026-05-11 17:18:41 +0200

    temporarily removed login, and changed the setting of Auction list

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -3,14 +3,15 @@ import { Route, Routes } from "react-router-dom";
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import Home from './pages/Home/Home';
-import Login from './pages/Login/Login';
 
 function App() {
   const [token, setToken] = useState("")
   const [isCollapsed, setIsCollapsed] = useState(false);
 
+  /* Bortkommentera det här för att slå på login igen...
   if (!token)
-    return (<Login setToken={setToken} />)
+  return (<Login setToken={setToken} />)
+  */
   return (
     <div className={`app-layout ${isCollapsed ? "sidebar-collapsed" : "sidebar-expanded"}`}>
       <NavBar isCollapsed={isCollapsed} onToggle={() => setIsCollapsed(!isCollapsed)} />
--- Frontend/src/pages/Home/Home.tsx
+++ Frontend/src/pages/Home/Home.tsx
@@ -1,22 +1,22 @@
 import { useEffect, useState } from "react";
-import AuctionCard from "../../components/AuctionCard/AuctionCard"
-import type {Auction} from "../../types/Types"; 
+import AuctionCard from "../../components/AuctionCard/AuctionCard";
 import getAllAuctions from "../../services/auctionService";
+import type { Auction } from "../../types/Types";
 
 const Home = () => {
-    const [auctions, setAuctions] = useState<Auction[]>([])
+    const [allAuctions, setAllAuctions] = useState<Auction[]>([])
+    const [userSpecificAuctions, setUserSpecificAuctions] = useState<Auction[]>([]);
     useEffect(() => {
-    
-        const fetchData = async () => {
-        try {
-        const response = await getAllAuctions();
-            setAuctions(response);
-        } catch (error) {
-        console.error("Error fetching data:", error);
-        }
-    };
 
-    fetchData();
+        const fetchData = async () => {
+            try {
+                const response = await getAllAuctions();
+                setAllAuctions(response);
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        };
+        fetchData();
     }, []);
 
     return (
@@ -27,10 +27,10 @@ const Home = () => {
             <main>
                 <section>
                     <h2>My auctions</h2>
-                    <AuctionCard AuctionList={auctions}/>
+                    <AuctionCard AuctionList={allAuctions} />
                 </section>
                 <section>
-                        <h2>New auctions</h2>
+                    <h2>New auctions</h2>
                 </section>
             </main>
         </>
