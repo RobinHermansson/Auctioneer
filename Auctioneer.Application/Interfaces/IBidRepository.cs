a8a08d135cdb92a91ccbd128a5a634387c697a46
robin.hermansson <robin.hermansson@hotmail.se>
2026-05-17 14:34:05 +0200

    Added a Bid repository, to add bids and fetch highest bid for an auction

---
+++ Auctioneer.Application/Interfaces/IBidRepository.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Entities;
+
+namespace Auctioneer.Application.Interfaces;
+
+public interface IBidRepository
+{
+    Task<decimal?> GetCurrentHighestBidAmountAsync(int auctionId);
+    Task<bool> AddBidAsync(Bid bid);
+
+}
---
+++ Infrastructure/Repositories/BidRepository.cs
@@ -0,0 +1,31 @@
+﻿using Auctioneer.Application.Interfaces;
+using Domain.Entities;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Auctioneer.Infrastructure.Repositories;
+
+public class BidRepository : IBidRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public BidRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<bool> AddBidAsync(Bid bid)
+    {
+        _context.Bids.Add(bid);
+        await _context.SaveChangesAsync();
+        return true;
+    }
+    public async Task<decimal?> GetCurrentHighestBidAmountAsync(int auctionId)
+    {
+        // Use an aggregate function (Max()) on the Bids DbSet
+        return await _context.Bids
+            .Where(b => b.AuctionId == auctionId)
+            .Select(b => b.Amount) // Select only the amount column
+            .MaxAsync();          // Execute the MAX aggregation query to the database
+    }
+}
