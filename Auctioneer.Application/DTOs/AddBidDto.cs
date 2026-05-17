637b92ce7946da7c36cd0cf6af2bae58c896e2a0
robin.hermansson <robin.hermansson@hotmail.se>
2026-05-17 14:36:00 +0200

    AddBidDto added

---
+++ Auctioneer.Application/DTOs/AddBidDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Auctioneer.Application.DTOs;
+
+public class AddBidDto
+{
+    public int BidderId { get; set; }
+    public int AuctionId { get; set; }
+    public int Amount { get; set; }
+}
