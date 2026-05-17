db65080a42f99977d35f06b144d05fe5fa1657b5
robin.hermansson <robin.hermansson@hotmail.se>
2026-05-17 14:35:49 +0200

    Added implementation to Gest highest bid and to add bid to an auction

--- Auctioneer.Application/Services/AuctionService.cs
+++ Auctioneer.Application/Services/AuctionService.cs
@@ -1,16 +1,21 @@
 ﻿using Auctioneer.Application.DTOs;
 using Auctioneer.Application.DTOs.Mapper;
 using Auctioneer.Application.Interfaces;
+using Domain.Entities;
 
 namespace Auctioneer.Application.Services;
 
 public class AuctionService
 {
     private readonly IAuctionRepository _repo;
+    private readonly IUserRepository _userRepository;
+    private readonly IBidRepository _bidRepository;
 
-    public AuctionService(IAuctionRepository repo)
+    public AuctionService(IAuctionRepository repo, IUserRepository userRepository, IBidRepository bidRepository)
     {
         _repo = repo;
+        _userRepository = userRepository;
+        _bidRepository = bidRepository;
     }
 
     public async Task<IEnumerable<AuctionDto>> GetAllAuctionsAsync()
@@ -36,4 +41,58 @@ public class AuctionService
         }
         return new AuctionDto();
     }
+     
+    public async Task<decimal?> GetHighestBidByAuctionId(int auctionId)
+    {
+         var auction = await _repo.GetAuctionByIdAsync(auctionId);
+        if (auction != null)
+        { 
+            var highestBid = await _bidRepository.GetCurrentHighestBidAmountAsync(auction.AuctionId);
+            return highestBid;
+        }
+        return 0;
+
+    }
+
+    public async Task<BidChangeResponseDto> AddBidAsync(AddBidDto bid)
+    {
+        var auction = await _repo.GetAuctionByIdAsync(bid.AuctionId);
+        if (auction != null)
+        {
+            if (auction.Owner.UserId == bid.BidderId)
+            {
+                return new BidChangeResponseDto() { Message = "The owner of the Auction can not bid.", Success = false };
+            }
+
+            var highestBid = await _bidRepository.GetCurrentHighestBidAmountAsync(bid.AuctionId);
+            if (highestBid >= bid.Amount) { 
+                return new BidChangeResponseDto() { Message = $"Bid was not higher than the highest bid: {highestBid}" , Success = false };
+            }
+
+            var bidderUser = await _userRepository.GetByIdAsync(bid.BidderId);
+            if (bidderUser == null)
+            {
+                return new BidChangeResponseDto() { Message = "The user does not exist." , Success = false };
+            }
+
+            try
+            {
+
+                var newBid = new Bid() { Amount = bid.Amount, AuctionId = bid.AuctionId, BidderUserId = bidderUser.UserId  };
+
+                auction.Bids.Add(newBid);
+                await _repo.UpdateAuctionAsync(auction);
+
+
+                return new BidChangeResponseDto() { Message = "Bid successful!" , Success = true };
+            }
+            catch (Exception ex)
+            {
+                return new BidChangeResponseDto() { Message = $"Failed to add bid: {ex.Message}", Success = false };
+            }
+            
+
+        }
+        return new BidChangeResponseDto() { Message = $"No auction with that Id: {bid.AuctionId} ", Success = false };
+    } 
 }
