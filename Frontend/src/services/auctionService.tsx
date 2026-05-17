a94f2baedd4e1b48e5a1dfd5b15e623380123955
robin.hermansson <robin.hermansson@hotmail.se>
2026-05-17 14:37:12 +0200

    Added function calls to Add bids to auction and getting highest bids

--- Frontend/src/services/auctionService.tsx
+++ Frontend/src/services/auctionService.tsx
@@ -1,3 +1,5 @@
+import type { AddBidRequest, AddBidResponse } from "../types/Types";
+
 export const getAllAuctions = async () => {
     const token = localStorage.getItem("token");
     const response = await fetch(
@@ -28,6 +30,34 @@ export const getAuctionById = async (id: number) => {
     const token = localStorage.getItem("token");
     const response = await fetch(
         `https://localhost:7029/api/Auction/${id}`,
+        {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }
+    );
+    return response.json();
+}
+
+export const addBidToAuction = async (bidRequest: AddBidRequest) : Promise<AddBidResponse> => {
+    const token = localStorage.getItem("token");
+    const response = await fetch(
+        `https://localhost:7029/api/Auction/bid`,
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`
+            },
+            body: JSON.stringify(bidRequest )
+        }
+    );
+    return response.json();
+}
+export const getHighestBidById = async (id: number) => {
+    const token = localStorage.getItem("token");
+    const response = await fetch(
+        `https://localhost:7029/api/Auction/highest/${id}`,
         {
             headers: {
                 Authorization: `Bearer ${token}`
