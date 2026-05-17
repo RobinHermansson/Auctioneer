5206c68743ecfe284b6db0d84e2b906fd81fdc55
robin.hermansson <robin.hermansson@hotmail.se>
2026-05-17 14:34:52 +0200

    Added a check to see which user id is logged in

---
+++ Auctioneer.API/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Auctioneer.API.Controllers;
+
+[Authorize]
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    [HttpGet("whoami")]
+    public async Task<ActionResult<int?>> WhoAmI()
+    {
+        var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+        if (!int.TryParse(userIdClaim, out var userId))
+        {
+            return Unauthorized();
+        }
+
+        return Ok(userId);
+    } 
+}
