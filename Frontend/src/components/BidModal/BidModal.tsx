5614461052fe424e410e52852662da5c8f0f5466
robin.hermansson <robin.hermansson@hotmail.se>
2026-05-17 14:38:52 +0200

    Connected all of the needed items to display price and to bid

--- Frontend/src/components/BidModal/BidModal.tsx
+++ Frontend/src/components/BidModal/BidModal.tsx
@@ -1,8 +1,30 @@
+import { useState } from "react";
 import "./BidModal.css";    
+import { addBidToAuction } from "../../services/auctionService";
 interface BidModalProps {
+    auctionCost: number;
+    auctionId : number;
+    userId: number;
     onClose: () => void;
 }
-const BidModal = ({ onClose }: BidModalProps) => {
+const BidModal = ({ auctionCost,auctionId, userId, onClose }: BidModalProps) => {
+    const [bidAmount, setBidAmount] = useState(auctionCost);
+    const [errorMessage, setErrorMessage] = useState("");
+    const [successMessage, setSuccessMessage] = useState("");
+    const onSubmitBid = async () => {
+        const response = await addBidToAuction({
+            bidderId: userId,
+            auctionId: auctionId,
+            amount: bidAmount
+        });
+        if (response.success) {
+            setSuccessMessage(response.message);
+            setErrorMessage("");
+        } else {
+            setErrorMessage(response.message);
+            setSuccessMessage("");
+        }
+    }
     return (
         <div className="modal-overlay">
             <div className="modal-content">
@@ -16,9 +38,19 @@ const BidModal = ({ onClose }: BidModalProps) => {
                 </div>
                     <div className="modal-bid-section">
 
-                        <p>Currently highest bid: </p>
-                        <input className="bid-input" type="number" placeholder="Enter your bid" />
-                        <button className="bid-button">Submit</button>
+                        <p>Currently highest bid: {auctionCost} SEK</p>
+                        <input 
+                            className="bid-input" 
+                            type="number" 
+                            placeholder="Enter your bid" 
+                            value={bidAmount}
+                            onChange={(e) => setBidAmount(Number(e.target.value))}
+                        />
+                        {errorMessage && <p className="error-message">{errorMessage}</p>}
+                        {successMessage && <p className="success-message">{successMessage}</p>}
+                        <button disabled={bidAmount <= auctionCost} className="bid-button" onClick={onSubmitBid}>
+                            Submit
+                        </button>
 
                     </div>
 
