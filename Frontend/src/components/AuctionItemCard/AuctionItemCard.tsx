123c6ffb06fd881b0c9bb72f4fd50b1893812987
robin.hermansson <robin.hermansson@hotmail.se>
2026-05-24 16:16:31 +0200

    Fixes so that the price is actually the highest bid, or the Price of the auction if no bid is present

--- Frontend/src/components/AuctionItemCard/AuctionItemCard.tsx
+++ Frontend/src/components/AuctionItemCard/AuctionItemCard.tsx
@@ -1,6 +1,8 @@
 import { useNavigate } from "react-router-dom";
 import type { AuctionItem } from "../../types/Types";
 import "./AuctionItemCard.css";
+import { useEffect, useState } from "react";
+import { getHighestBidById } from "../../services/auctionService";
 
 interface AuctionItemCardProps {
     AuctionItem: AuctionItem
@@ -8,10 +10,18 @@ interface AuctionItemCardProps {
 }
 const AuctionItemCard = ({ AuctionItem, AuctionId }: AuctionItemCardProps) => {
     const navigate = useNavigate()
+    const [auctionItemPrice, setAuctionItemPrice] = useState<number>(AuctionItem.price);
 
     const goAuction = () => {
         navigate(`/auction/${AuctionId}`)
     }
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await getHighestBidById(AuctionId);
+            setAuctionItemPrice(response ? response : AuctionItem.price);
+        };
+        fetchData();
+    }, [AuctionItem])
     return (
         <li className="auction-item-card" onClick={goAuction}>
             <img
@@ -21,7 +31,7 @@ const AuctionItemCard = ({ AuctionItem, AuctionId }: AuctionItemCardProps) => {
             <h4>{AuctionItem.name}</h4>
             <p className="description">{AuctionItem.description}</p>
             <p className="type">{AuctionItem.auctionType}</p>
-            <p className="price">{AuctionItem.price} SEK</p>
+            <p className="price">{auctionItemPrice} SEK</p>
         </li>
     )
 }
