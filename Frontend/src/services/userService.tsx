35f47888fb0309be4abc395f94725398e18aaac9
robin.hermansson <robin.hermansson@hotmail.se>
2026-05-24 15:03:17 +0200

    Reapply "Adds AddUserAsync contract"
    This reverts commit 2f229a92d3c9987a465024c395f1f38e7523c3ca.

--- Frontend/src/services/userService.tsx
+++ Frontend/src/services/userService.tsx
@@ -1,10 +1,22 @@
-const whoAmI = async () => {
+import type { RegisterUserRequest, RegisterUserResponse } from "../types/Types";
+
+export const whoAmI = async () => {
     const token = localStorage.getItem("token");
     const response = await fetch("https://localhost:7029/api/User/whoami", {
         headers: {
             Authorization: `Bearer ${token}`}
     });
     return response.json();
-}
+};
 
-export default whoAmI;
+const registerUser = async (RegisterRequest: RegisterUserRequest): Promise<RegisterUserResponse> => {
+    const response = await fetch("https://localhost:7029/api/User/register", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(RegisterRequest)
+    });
+    return response.json();
+};
+export default registerUser;
