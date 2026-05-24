b921dc430d619ed10d4f040d328dcdc20f133931
robin.hermansson <robin.hermansson@hotmail.se>
2026-05-24 13:10:28 +0200

    Removes the login page the login process from App.tsx

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -4,25 +4,21 @@ import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import Auction from './pages/Auction/Auction';
 import Home from './pages/Home/Home';
-import Login from './pages/Login/Login';
 import CreateAuction from './pages/CreateAuction/CreateAuction';
 
 function App() {
-  const [token, setToken] = useState("")
   const [isCollapsed, setIsCollapsed] = useState(false);
 
-  if (!token)
-    return (<Login setToken={setToken} />)
 
   return (
     <div className={`app-layout ${isCollapsed ? "sidebar-collapsed" : "sidebar-expanded"}`}>
       <NavBar isCollapsed={isCollapsed} onToggle={() => setIsCollapsed(!isCollapsed)} />
       <main className="main-content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/auction/:auctionId" element={<Auction />} />
-          <Route path="/create-auction" element={<CreateAuction />} />
-        </Routes>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/auction/:auctionId" element={<Auction />} />
+            <Route path="/create-auction" element={<CreateAuction />} />
+          </Routes>
       </main>
     </div>
   )
