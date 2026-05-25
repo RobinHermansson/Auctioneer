140ac9616024592f82d9f0de0de55f12552415b9
robin.hermansson <robin.hermansson@hotmail.se>
2026-05-25 18:08:41 +0200

    Adds DeleteAuctionResponseDto

---
+++ Auctioneer.Application/DTOs/DeleteAuctionResponseDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Auctioneer.Application.DTOs;
+
+public class DeleteAuctionResponseDto
+{
+    public bool Success { get; set; }
+    public string Message { get; set; } = string.Empty;
+}
