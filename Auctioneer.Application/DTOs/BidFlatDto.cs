0d76c052b7aeeb229fe5ce44895536c78f3af090
robin.hermansson <robin.hermansson@hotmail.se>
2026-05-27 17:29:52 +0200

    Add a timestamp to the BidFlatDto

---
+++ Auctioneer.Application/DTOs/BidFlatDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Auctioneer.Application.DTOs;
+
+ public class BidFlatDto
+{
+    public int BidId { get; set; }
+    public decimal Amount { get; set; }
+    public int BidderId { get; set; }
+    public int AuctionId { get; set; }
+    public DateTime Timestamp { get; set; }
+
+}
