868628efe83f7159991f63837acf1aae5b57ed8b
robin.hermansson <robin.hermansson@hotmail.se>
2026-05-28 15:35:00 +0200

    Adds a dto to transfer all user data

---
+++ Auctioneer.Application/DTOs/UserFullFlatDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Auctioneer.Application.DTOs;
+
+public class UserFullFlatDto
+{
+    public int UserId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public bool IsAdmin { get; set; }
+    public bool IsActive { get; set; }
+}
