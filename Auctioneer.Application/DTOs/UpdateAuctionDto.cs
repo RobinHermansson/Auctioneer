02a65707759f92395bcec435f7a844efc3092998
robin.hermansson <robin.hermansson@hotmail.se>
2026-05-28 14:06:10 +0200

    Adds a UpdateAuctionDto

---
+++ Auctioneer.Application/DTOs/UpdateAuctionDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Auctioneer.Application.DTOs;
+
+public class UpdateAuctionDto
+{
+
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public decimal StartingPrice { get; set; }
+    public string ItemName { get; set; }
+    public string AuctionType { get; set; }
+}
+
