d7526b6f8fb66ea10418d8d9aa0f414f14a95e8f
robin.hermansson <robin.hermansson@hotmail.se>
2026-05-28 08:09:11 +0200

    Adds an extension method to clean up the check whether a user has access

---
+++ Auctioneer.API/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace Auctioneer.API.Extensions;
+
+public static class ClaimsPrincipalExtensions
+{
+    public static int? GetUserId(this ClaimsPrincipal user)
+    {
+        var claim = user.FindFirst(JwtRegisteredClaimNames.Sub)?.Value;
+        return int.TryParse(claim, out var userId) ? userId : null;
+    }
+}
