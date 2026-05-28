065f52590e8b6d02173efdb1d088760d8a93a48e
robin.hermansson <robin.hermansson@hotmail.se>
2026-05-28 15:35:18 +0200

    Adds the contract for getting all users

--- Auctioneer.Application/Interfaces/IUserRepository.cs
+++ Auctioneer.Application/Interfaces/IUserRepository.cs
@@ -4,6 +4,7 @@ namespace Auctioneer.Application.Interfaces;
 
 public interface IUserRepository
 {
+    Task<List<User>> GetAllUsersAsync();
     Task<User?> GetByEmailAsync(string email);
     Task<User?> GetByIdAsync(int id);
     Task<bool> AddUserAsync(User user);
