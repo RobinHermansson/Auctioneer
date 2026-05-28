0eea3ed52fd7065f3d77f3a0d5a3e03d3e2ab8cd
robin.hermansson <robin.hermansson@hotmail.se>
2026-05-28 15:36:12 +0200

    Adds an Admin endpoint to get all users with.

---
+++ Auctioneer.API/Controllers/AdminController.cs
@@ -0,0 +1,27 @@
+﻿using Auctioneer.API.Extensions;
+using Auctioneer.Application.DTOs;
+using Auctioneer.Application.Interfaces;
+using Auctioneer.Application.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Auctioneer.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AdminController : ControllerBase
+{
+    private readonly UserService _userService;
+    public AdminController(UserService userService)
+    {
+        _userService = userService;    
+    }
+
+    [Authorize(Roles = "Admin")]
+    [HttpGet("users")]
+    public async Task<ActionResult<IEnumerable<UserDto?>>> GetAllUsers()
+    {
+
+        return Ok(await _userService.GetAllUsersAsync());
+    }
+}
