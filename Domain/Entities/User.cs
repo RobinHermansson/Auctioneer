34ad7fe5c5a96987a6819fa4d2cbe3536b8ac05d
robin.hermansson <robin.hermansson@hotmail.se>
2026-05-28 15:35:29 +0200

    Adds the flag "IsActive"

--- Domain/Entities/User.cs
+++ Domain/Entities/User.cs
@@ -9,6 +9,7 @@ public class User
     public string Email { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
     public int UserRoleId { get; set; }
+    public bool IsActive { get; set; }
     public UserRole UserRole { get; set; }
     public ICollection<Bid> Bids { get; set; } = new List<Bid>();
 
